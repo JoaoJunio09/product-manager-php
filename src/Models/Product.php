7f0d1032b2a85f6e1a2e545bfd8ce274e5d636a9
joao_junio <joaojunio818@gmail.com>
2026-02-21 15:53:21 -0300

    Criando model: Product em php

--- src/Models/Product.php
+++ src/Models/Product.php
@@ -1,17 +1,29 @@
 <?php
 
-class Product 
+class Product
 {
-	public int $id;
-	public string $name;
-	public string $price;
-	public function __construct(
-		int $id, 
-		string $name, 
-		string $price
-	) {
+	private $id;
+	private $name;
+	private $price;
+	private $amount;
+	private $status;
+
+	public function __construct($id, $name, $price, $amount, $status)
+	{
 		$this->id = $id;
 		$this->name = $name;
 		$this->price = $price;
+		$this->amount = $amount;
+		$this->status = $status;
 	}
+
+	public function getId(): int { return $this->id; }
+
+	public function getName(): string { return $this->name; }
+
+	public function getPrice(): float { return $this->price; }
+
+	public function getAmount(): int { return $this->amount; }
+
+	public function getStatus(): string { return $this->status; }
 }
