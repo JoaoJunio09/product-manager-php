6e8676536770b4506a1efa3d53908ea98c69cda8
joao_junio <joaojunio818@gmail.com>
2026-02-21 15:41:19 -0300

    realizando conexão com banco de dados

---
+++ src/Core/Database.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Core;
+
+use PDO;
+
+class Database
+{
+	private static $connection = null;
+
+	public static function getConnection()
+	{
+		if (self::$connection === null) {
+			$config = require __DIR__ . '/../../config/database.php';
+
+			self::$connection = new PDO(
+				"mysql:host={$config['host']};dbname={$config['dbname']}",
+				$config['user'],
+				$config['password']
+			);
+
+			self::$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		}
+
+		return self::$connection;
+	}
+}
